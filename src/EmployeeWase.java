a786024324a4aa92fc6e0a0f605f835fd5e26650
neeteshgolhani <neeteshgolhani4@gmail.com>
2023-04-17 23:45:00 +0530

    Added: Calculate Daily Wase Employee Using Switch Case monthly

--- src/EmployeeWase.java
+++ src/EmployeeWase.java
@@ -2,27 +2,39 @@ import java.util.Random;
 public class EmployeeWase {
     public static final int FULL_TIME = 2;
     public static final int PART_TIME = 1;
+    public static final int WORKING_DAYS = 20;
+    public static final int WAGE_PER_HR = 20;
     public static void main(String[] args) {
         System.out.println("Welcome to the EmployeeWageComputationalProgram");
         Random random = new Random();
         int present = random.nextInt(3);
         //Check if employee is present
+        int totalWage = 0;
         int WagePerHr = 20;
-        int dailyWage, workingHrs;
-        switch (present){
-            case FULL_TIME :
-                workingHrs = 8;
-                System.out.println("Employee Working Fullday");
-                break;
-            case PART_TIME:
-                workingHrs =4;
-                System.out.println("Employee Working Part Time");
-                break;
-            default:
-                workingHrs = 0;
-                System.out.println("Employee Absent");
+        int dailyWage;
+
+        for (int day = 1; day <= WORKING_DAYS; day++) {
+            present = random.nextInt(3);
+            int workingHrs = 0;
+
+            switch (present) {
+                case FULL_TIME:
+                    workingHrs = 8;
+                    System.out.println("Employee Working Fullday");
+                    break;
+                case PART_TIME:
+                    workingHrs = 4;
+                    System.out.println("Employee Working Part Time");
+                    break;
+                default:
+                    workingHrs = 0;
+                    System.out.println("Employee Absent");
+            }
+
+            dailyWage = workingHrs * WAGE_PER_HR;
+            System.out.println("Day " + day + " wage is:" + dailyWage);
+            totalWage += dailyWage;
         }
-        dailyWage = workingHrs * WagePerHr;
-        System.out.println("Daily wage is " + dailyWage);
+        System.out.println("Total wage for a month is " + totalWage);
     }
 }
