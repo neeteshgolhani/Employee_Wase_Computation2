c29a039354762c2f343d3368a570db17ae833797
neeteshgolhani <neeteshgolhani4@gmail.com>
2023-04-17 23:37:49 +0530

    Added: Calculate Daily Wase Employee Part Time

--- src/EmployeeWase.java
+++ src/EmployeeWase.java
@@ -3,15 +3,24 @@ public class EmployeeWase {
     public static void main(String[] args) {
         System.out.println("Welcome to the EmployeeWageComputationalProgram");
         Random random = new Random();
-        int present = random.nextInt(2);
+        int present = random.nextInt(3);
         //Check if employee is present
-        int WagePerHr =20;
-        int wrkHrs = 8;
-        int dailyWage = present * WagePerHr * wrkHrs;
-
-        if (present == 1)
-            System.out.println("Employee is present and Daily Wage Is " + dailyWage);
-        else
-            System.out.println("Employee is absent so Daily Wage is "+ dailyWage);
+        int WagePerHr = 20;
+        int dailyWage, workingHrs;
+        if (present == 0) {
+            //Employee is absent
+            workingHrs = 0;
+            System.out.println("Employee is absent");
+        } else if (present == 1) {
+            //Employee working as part-time
+            workingHrs = 8;
+            System.out.println("Employee working as part-time");
+        } else {
+            //Employee working as full time
+            workingHrs = 16;
+            System.out.println("Employee working as full time");
+        }
+        dailyWage = workingHrs * WagePerHr;
+        System.out.println("Daily wage is " + dailyWage);
     }
 }
