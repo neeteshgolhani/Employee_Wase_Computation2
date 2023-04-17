98abbd68ac9320950f2e41cf18d544db0d1c9114
neeteshgolhani <neeteshgolhani4@gmail.com>
2023-04-18 00:34:42 +0530

    Added: Calculate Daily Wase Employee Using Switch Case monthly Till Days20 And 100Hours

--- src/EmployeeWase.java
+++ src/EmployeeWase.java
@@ -2,20 +2,23 @@ import java.util.Random;
 public class EmployeeWase {
     public static final int FULL_TIME = 2;
     public static final int PART_TIME = 1;
-    public static final int WORKING_DAYS = 20;
     public static final int WAGE_PER_HR = 20;
+    public static final int MAX_WORKING_HRS = 100;
+
     public static void main(String[] args) {
         System.out.println("Welcome to the EmployeeWageComputationalProgram");
         Random random = new Random();
-        int present = random.nextInt(3);
+        int isPresent = random.nextInt(3);
         //Check if employee is present
         int totalWage = 0;
-        int WagePerHr = 20;
         int dailyWage;
+        int workingHrs = 0;
+        int day = 1, totalWorkingHrs = 0;
 
-        for (int day = 1; day <= WORKING_DAYS; day++) {
-            present = random.nextInt(3);
-            int workingHrs = 0;
+        for (day = 1, totalWorkingHrs = 0; day <= MAX_WORKING_HRS
+                && totalWorkingHrs < MAX_WORKING_HRS; day++, totalWorkingHrs += workingHrs) {
+            int present = random.nextInt(3);
+
 
             switch (present) {
                 case FULL_TIME:
@@ -29,12 +32,14 @@ public class EmployeeWase {
                 default:
                     workingHrs = 0;
                     System.out.println("Employee Absent");
+                    break;
             }
 
             dailyWage = workingHrs * WAGE_PER_HR;
-            System.out.println("Day " + day + " wage is:" + dailyWage);
+            System.out.println("Day " + day + " workingHrs is " + workingHrs + " wage is: " + dailyWage);
             totalWage += dailyWage;
         }
         System.out.println("Total wage for a month is " + totalWage);
+
     }
 }
