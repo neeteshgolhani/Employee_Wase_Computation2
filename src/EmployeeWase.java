e4f5063f2cacd905676d4a935f0441cf39405427
neeteshgolhani <neeteshgolhani4@gmail.com>
2023-04-17 23:34:28 +0530

    Added: Calculate Daily Wase Employee Per Hours

--- src/EmployeeWase.java
+++ src/EmployeeWase.java
@@ -1,14 +1,17 @@
 import java.util.Random;
-
 public class EmployeeWase {
     public static void main(String[] args) {
         System.out.println("Welcome to the EmployeeWageComputationalProgram");
         Random random = new Random();
         int present = random.nextInt(2);
         //Check if employee is present
+        int WagePerHr =20;
+        int wrkHrs = 8;
+        int dailyWage = present * WagePerHr * wrkHrs;
+
         if (present == 1)
-            System.out.println("Employee is present.");
+            System.out.println("Employee is present and Daily Wage Is " + dailyWage);
         else
-            System.out.println("Employee is absent.");
+            System.out.println("Employee is absent so Daily Wage is "+ dailyWage);
     }
 }
